95f8b79c03f1c00f8a128fc94600ef9016e28b37
Savvas Chanlaridis <schanlar@astro.uni-bonn.de>
2019-04-08 13:11:24 +0200

    Copy MESA history + final profile from Condor to local PC

---
+++ data_analysis/fetch_data.py
@@ -0,0 +1,68 @@
+# PATHS
+grid_dir = '/vol/hal/halraid/jantoniadis/HeCores/Condor/full_grid'
+out_dir = '/vol/aibn1107/data2/schanlar/HeCoresCondor/full_data'
+
+import glob
+import os, sys
+
+sys.stdout = open('fetch_data_log.txt', 'wt')
+
+# Make folder to keep data
+if not os.path.exists(out_dir):
+    os.chdir('/vol/aibn1107/data2/schanlar/HeCoresCondor')
+    print('Creating folder...')
+    os.system('mkdir full_data')
+
+
+# Fetch from original path
+model_paths = [path for path in glob.glob(grid_dir + '/*')]
+
+historyCounter = 0
+historyExists = 0
+profileCounter = 0
+profileExists = 0
+
+
+for path in model_paths:
+
+    logs_path = os.path.join(path,'LOGS')
+    if os.path.isfile(logs_path + '/history.data'):
+        historyCounter += 1
+        historyExists += 1
+
+        folder_name = path[-20:]
+        os.chdir(out_dir)
+        os.system('mkdir ' + folder_name)
+        new_model_path = os.path.join(out_dir, folder_name)
+
+        os.chdir(logs_path)
+        print('Processing...')
+        os.system('cp history.data ' + new_model_path)
+        print('History file #'+ str(historyCounter) + ' in:', folder_name, 'created!')
+
+
+    else:
+        historyCounter += 1
+        print('History file #'+ str(historyCounter) + ' from:', folder_name, 'was not found!')
+
+
+
+    if os.path.isfile(path + '/final_profile.data'):
+        profileCounter += 1
+        profileExists += 1
+
+        os.chdir(path)
+        os.system('cp final_profile.data ' + new_model_path)
+        print('Final profile #' + str(profileCounter) + ' in:', folder_name, 'created')
+
+
+    else:
+        profileCounter += 1
+        print('Final profile #' + str(profileCounter) + ' from:', folder_name, 'was not found!')
+
+
+
+print(str(historyExists), 'history files ' + 'out of ' + str(historyCounter) + ' were copied from', grid_dir)
+print(str(profileExists), 'profile files ' + 'out of ' + str(profileCounter) + ' were copied from', grid_dir)
+
+sys.stdout.close()
